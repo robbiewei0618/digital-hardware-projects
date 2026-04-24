69d405217d828c27688eef69e3532c2801694e8d
robbiewei0618 <robbiewei0618@gmail.com>
2026-04-25 00:08:10 +0800

    Delete FLP and FXP Adder and Multiplier/ALU_HW1_TA_pre_sim.sh

--- FLP and FXP Adder and Multiplier/ALU_HW1_TA_pre_sim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/tcsh
-
-vcs -R -debug_access+all \
-/MasterClass/M133040086_ALU/HW1/pre_sim/ALU_HW1_testbench.v \
-/MasterClass/M133040086_ALU/HW1/RTL/FLP_adder/FLP_adder.v \
-/MasterClass/M133040086_ALU/HW1/RTL/FLP_adder_4/FLP_adder_4.v \
-/MasterClass/M133040086_ALU/HW1/RTL/FLP_adder_7/FLP_adder_7.v \
-+full64 \
-+access+r +vcs+fsdbon +fsdb+mda +fsdbfile+FLP.fsdb +v2k

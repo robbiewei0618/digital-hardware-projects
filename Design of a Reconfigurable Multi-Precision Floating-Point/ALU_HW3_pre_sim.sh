6436104c583b0235784adb4e893877853386475c
robbiewei0618 <robbiewei0618@gmail.com>
2026-04-24 23:20:45 +0800

    Delete Design of a Reconfigurable Multi-Precision Floating-Point/ALU_HW3_pre_sim.sh

--- Design of a Reconfigurable Multi-Precision Floating-Point/ALU_HW3_pre_sim.sh
+++
@@ -1,8 +0,0 @@
-
-
-vcs -R -debug_access+all \
-/MasterClass/M133040086_ALU/HW3/RTL_SIM/DP4_testbench.v\
-/MasterClass/M133040086_ALU/HW3/RTL/DP4_pipeline_CG.v \
-+full64 \
-+access+r +vcs+fsdbon +fsdb+mda +fsdbfile+DP4_pipeline_CG.fsdb +v2k
-
